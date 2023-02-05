d88ba316c98960f88bf6776bc0318b47bf1f3b53
JaredMathis <jared.mathis@native.consulting>
2023-02-04 20:07:02 -0500

    function_auto graph_cycle_new

--- src/graph/cycle/new.mjs
+++ src/graph/cycle/new.mjs
@@ -1,7 +1,9 @@
+import { list_copy } from './../../../node_modules/m00/src/list/copy.mjs';
 import { range } from './../../../node_modules/m00/src/range.mjs';
 import { arguments_assert } from './../../../node_modules/m00/src/arguments/assert.mjs';
 import { number_at_least } from './../../../node_modules/m00/src/number/at/least.mjs';
 export function graph_cycle_new(vertex_count) {
     arguments_assert(arguments, number_at_least(3));
     let vertices = range(vertex_count);
+    let shuffled = list_copy(vertices);
 }
