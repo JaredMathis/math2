c1c1d43ae2ff71782ba292f58c92714b86bc2339
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:46:00 -0500

    function_auto graph_cycle_new

--- src/graph/cycle/new.mjs
+++ src/graph/cycle/new.mjs
@@ -8,7 +8,7 @@ export function graph_cycle_new(vertex_count) {
     arguments_assert(arguments, number_at_least(3));
     let result = [];
     let vertices = range(vertex_count);
-    let shuffled = list_copy_shuffle(shuffled);
+    let shuffled = list_copy_shuffle(vertices);
     for_each(shuffled, (current, key) => {
         let next = list_get_wrapped(shuffled, parseInt(key, 10) + 1);
         result.push([
