070b45de36f0e546305b9e72de09209099711e86
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:37:44 -0500

    function_auto graph_tree_new

--- src/graph/tree/new.mjs
+++ src/graph/tree/new.mjs
@@ -1,2 +1,4 @@
-export function graph_tree_new() {
+import { range } from './../../../node_modules/m00/src/range.mjs';
+export function graph_tree_new(vertex_count) {
+    let vertices = range(vertex_count);
 }
