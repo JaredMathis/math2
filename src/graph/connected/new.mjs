b2f5a3764ca3810a7bfbb4696b7df64bdf1f3931
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:01:11 -0500

    function_auto graph_connected_new

--- src/graph/connected/new.mjs
+++ src/graph/connected/new.mjs
@@ -1,2 +1,5 @@
-export function graph_connected_new() {
+import { arguments_assert } from './../../../node_modules/m00/src/arguments/assert.mjs';
+import { number_positive_is } from './../../../node_modules/m00/src/number/positive/is.mjs';
+export function graph_connected_new(vertex_count) {
+    arguments_assert(arguments, number_positive_is);
 }
