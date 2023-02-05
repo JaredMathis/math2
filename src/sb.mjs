b66eecb2daacf2113e099bcda9475674c321a2ed
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:33:14 -0500

    function_auto sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,10 +1,18 @@
-import { graph_connected_new } from "./graph/connected/new.mjs";
-import { graph_cycle_new } from "./graph/cycle/new.mjs";
-import { graph_neighbors_get } from "./graph/neighbors/get.mjs";
-import { graph_tree_new } from "./graph/tree/new.mjs";
-
+import { log } from './../node_modules/m00/src/log.mjs';
+import { list_first } from './../node_modules/m00/src/list/first.mjs';
+import { graph_neighbors_get } from './graph/neighbors/get.mjs';
+import { graph_traverse_tree } from './graph/traverse/tree.mjs';
+import { graph_tree_new } from './graph/tree/new.mjs';
 export function sb() {
-    let g = graph_connected_new(5)
+    let size = 50;
+    let g = graph_tree_new(size);
     console.log(g);
-    console.log(graph_neighbors_get(g, 0));
+    let found = [];
+    let current = 0;
+    let previous = list_first(graph_neighbors_get(g));
+    while (found.length < size) {
+        let next_current = graph_traverse_tree(g, previous, current);
+        previous = current;
+        current = next_current;
+    }
 }
