c49e26a820edf73c1a4b8280994f7be927d3c558
JaredMathis <jared.mathis@native.consulting>
2023-02-04 19:54:22 -0500

    function_auto graph_cycle_new

--- src/graph/cycle/new.mjs
+++ src/graph/cycle/new.mjs
@@ -1,2 +1,5 @@
-export function graph_cycle_new() {
+import { arguments_assert } from './../../../node_modules/m00/src/arguments/assert.mjs';
+import { number_at_least } from './../../../node_modules/m00/src/number/at/least.mjs';
+export function graph_cycle_new(vertex_count) {
+    arguments_assert(arguments, number_at_least(3));
 }
