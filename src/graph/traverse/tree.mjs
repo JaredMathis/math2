44d0c336943041f8a7f0902a97f4616dc8c7a510
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:30:20 -0500

    function_auto graph_traverse_tree

--- src/graph/traverse/tree.mjs
+++ src/graph/traverse/tree.mjs
@@ -1,3 +1,9 @@
+import { graph_neighbors_get } from './../neighbors/get.mjs';
+import { list_min } from './../../../node_modules/m00/src/list/min.mjs';
 export function graph_traverse_tree(graph, previous, current) {
     let neighbors = graph_neighbors_get(graph, current);
+    let neighbors_larger = neighbors.filter(n => n > previous);
+    if (neighbors_larger.length) {
+        return list_min(neighbors_larger);
+    }
 }
