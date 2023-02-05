e104f257239697bf28bac09c26b325a4d2e21502
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:58:47 -0500

    function_auto graph_random_new

--- src/graph/random/new.mjs
+++ src/graph/random/new.mjs
@@ -1,3 +1,4 @@
+import { list_add } from './../../../node_modules/m00/src/list/add.mjs';
 import { for_each_nested } from './../../../node_modules/m00/src/for/each/nested.mjs';
 import { arguments_assert } from './../../../node_modules/m00/src/arguments/assert.mjs';
 import { number_positive_is } from './../../../node_modules/m00/src/number/positive/is.mjs';
@@ -5,6 +6,15 @@ import { range } from './../../../node_modules/m00/src/range.mjs';
 export function graph_random_new(vertex_count) {
     arguments_assert(arguments, number_positive_is);
     let vertices = range(vertex_count);
-    for_each_nested(vertices, v => {
+    let result = [];
+    for_each_nested(vertices, (v, w) => {
+        if (v < w) {
+            if (Math.random() < 0.5) {
+                list_add(result, [
+                    v,
+                    w
+                ]);
+            }
+        }
     });
 }
