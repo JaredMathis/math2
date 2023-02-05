19bfefdcd0f9ac9b0e158bb94a0170774d5fe521
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:53:03 -0500

    function_auto graph_random_new

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,5 +1,6 @@
 import { graph_cycle_new } from "./graph/cycle/new.mjs";
+import { graph_tree_new } from "./graph/tree/new.mjs";
 
 export function sb() {
-    console.log(graph_cycle_new(5));
+    console.log(graph_tree_new(5));
 }
