6097b990b618e39311b0ba1ed8e038dc22b2d4d8
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:41:11 -0500

    function_auto graph_tree_new

--- src/graph/tree/new.mjs
+++ src/graph/tree/new.mjs
@@ -1,7 +1,13 @@
+import { list_copy } from './../../../node_modules/m00/src/list/copy.mjs';
+import { list_shuffle } from './../../../node_modules/m00/src/list/shuffle.mjs';
+import { list_copy } from 'm00/src/list/copy.mjs';
+import { list_shuffle } from 'm00/src/list/shuffle.mjs';
 import { arguments_assert } from './../../../node_modules/m00/src/arguments/assert.mjs';
 import { number_positive_is } from './../../../node_modules/m00/src/number/positive/is.mjs';
 import { range } from './../../../node_modules/m00/src/range.mjs';
 export function graph_tree_new(vertex_count) {
     arguments_assert(arguments, number_positive_is);
     let vertices = range(vertex_count);
+    let remaining = list_copy(vertices);
+    list_shuffle(vertices);
 }
