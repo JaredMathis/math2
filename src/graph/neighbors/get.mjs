7c1461a95957696c249e3016e281f3b1976da84d
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:09:32 -0500

    function_auto graph_neighbors_get

--- src/graph/neighbors/get.mjs
+++ src/graph/neighbors/get.mjs
@@ -1,2 +1,25 @@
-export function graph_neighbors_get() {
+import { for_each } from './../../../node_modules/m00/src/for/each.mjs';
+import { list_add_if_not_exists } from './../../../node_modules/m00/src/list/add/if/not/exists.mjs';
+import { for_each } from 'm00/src/for/each.mjs';
+export function graph_neighbors_get(graph, v) {
+    let neighbors = [];
+    for_each(graph, edge => {
+        for_each([
+            [
+                0,
+                1
+            ],
+            [
+                1,
+                0
+            ]
+        ], a => {
+            let [b, c] = a;
+            if (edge[b] === v) {
+                const n = edge[c];
+                list_add_if_not_exists(neighbors, n);
+            }
+        });
+    });
+    return neighbors;
 }
